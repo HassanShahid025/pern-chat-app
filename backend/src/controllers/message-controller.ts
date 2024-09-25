1c4a082c154dc71ad359ecbec97d9b92fa8b0bb1
hassan shahid <hassanshahid234@gmail.com>
2024-09-25 14:14:38 +0500

    messages route completed

---
+++ backend/src/controllers/message-controller.ts
@@ -0,0 +1,120 @@
+import { Request, Response } from "express";
+import prisma from "../db/prisma.js";
+
+export const sendMessage = async (req: Request, res: Response) => {
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user.id;
+
+    let conversation = await prisma.conversation.findFirst({
+      where: {
+        participantIds: {
+          hasEvery: [senderId, receiverId],
+        },
+      },
+    });
+
+    // the very first message is being sent, that's why we need to create a new conversation
+    if (!conversation) {
+      conversation = await prisma.conversation.create({
+        data: {
+          participantIds: {
+            set: [senderId, receiverId],
+          },
+        },
+      });
+    }
+
+    const newMessage = await prisma.message.create({
+      data: {
+        senderId,
+        body: message,
+        conversationId: conversation.id,
+      },
+    });
+
+    // This means that the newMessage (which already exists in the messages table) is being associated with the conversation by adding its id to the messages list of that conversation. This is done by updating the conversation record in the conversations table.
+    if (newMessage) {
+      conversation = await prisma.conversation.update({
+        where: {
+          id: conversation.id,
+        },
+        data: {
+          messages: {
+            connect: {
+              id: newMessage.id,
+            },
+          },
+        },
+      });
+    }
+
+    // Socket io will go here
+    // const receiverSocketId = getReceiverSocketId(receiverId);
+
+    // if (receiverSocketId) {
+    //   io.to(receiverSocketId).emit("newMessage", newMessage);
+    // }
+
+    res.status(201).json(newMessage);
+  } catch (error: any) {
+    console.error("Error in sendMessage: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req: Request, res: Response) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user.id;
+
+    const conversation = await prisma.conversation.findFirst({
+      where: {
+        participantIds: {
+          hasEvery: [senderId, userToChatId],
+        },
+      },
+      include: {
+        messages: {
+          orderBy: {
+            createdAt: "asc",
+          },
+        },
+      },
+    });
+
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+
+    res.status(200).json(conversation.messages);
+  } catch (error: any) {
+    console.log("Error in sendMessage", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getUserAllConversations = async (req: Request, res: Response) => {
+  try {
+    const authUserId = req.user.id;
+
+    const users = await prisma.user.findMany({
+      where: {
+        id: {
+          not: authUserId,
+        },
+      },
+      select: {
+        id: true,
+        fullName: true,
+        profilePic: true,
+      },
+    });
+
+    res.status(200).json(users);
+  } catch (error) {
+    console.log("Error in getAllConversations", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/src/routes/message-route.ts
+++ backend/src/routes/message-route.ts
@@ -1,17 +1,12 @@
 import express from 'express';
+import protectRoute from '../middleware/protectRoute.js';
+import { getMessages, getUserAllConversations, sendMessage } from '../controllers/message-controller.js';
+
 
 const router = express.Router();
 
-router.get('/messages', (req, res) => {
-  res.send('Messages');
-});
-
-router.get('/messages/:id', (req, res) => {
-  res.send('Message ' + req.params.id);
-});
-
-router.post('/messages', (req, res) => {
-  res.send('Message created');
-});
+router.get('/getMessages/:id',protectRoute,getMessages)
+router.get('/getUserAllConversations',protectRoute,getUserAllConversations)
+router.post('/send/:id',protectRoute,sendMessage)
 
 export default router;
