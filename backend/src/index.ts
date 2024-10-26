628fb12014fe7f930dc597f10df86664052ef511
hassan shahid <hassanshahid234@gmail.com>
2024-10-26 16:20:56 +0500

    commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,22 @@
-import express from 'express';
-import authRoutes from './routes/auth-route.js';
-import messageRoutes from './routes/message-route.js';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import { app, server } from './socket/socket.js'; //For socket.io
+import express from "express";
+import authRoutes from "./routes/auth-route.js";
+import messageRoutes from "./routes/message-route.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { app, server } from "./socket/socket.js"; //For socket.io
 
 dotenv.config();
 // const app = express(); //For normal ts backend without socket.io
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('Hello World 2');
+app.get("/", (req, res) => {
+  res.send("Hello World 2");
 });
 
-app.use("/api/v1/auth",authRoutes)
-app.use("/api/v1/messages",messageRoutes)
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/messages", messageRoutes);
 
 const PORT = process.env.PORT || 3000;
 
@@ -24,9 +24,7 @@ server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-
 //For normal ts backend without socket.io
 // app.listen(PORT, () => {
 //   console.log(`Server is running on http://localhost:${PORT}`);
 // });
- 
